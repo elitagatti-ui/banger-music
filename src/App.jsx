c7a6f6ccb19a2690d4751e2db8427b1c66333c74
ntfran <francisco.sandoval7700@gmail.com>
2026-08-23 06:08:45 -0300

    enrutar page admin en el navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Detalle from "./pages/Detalle";
 import Error from "./pages/PaginaError";
 import Login from "./pages/Login"
 import Registro from "./pages/Registro";
+import Admin from "./pages/Admin";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/detalle/:id" element={<Detalle />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
+        <Route path="/admin" element={<Admin />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Admin.jsx
@@ -0,0 +1,10 @@
+const Admin = () => {
+  return (
+    <div className="container py-5 text-white">
+      <h2>Panel de Administración</h2>
+      <p>Bienvenido, Admin. Vista en construcción.</p>
+    </div>
+  );
+};
+
+export default Admin;
